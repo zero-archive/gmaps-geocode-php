691b2d23b9d91b2dd6a08da1535cd2971cd92696
dotzero <mail@dotzero.ru>
2016-04-11 22:36:01 +0300

    Update examples

--- examples/example.php
+++ examples/example.php
@@ -1,41 +1,22 @@
-<html>
-<head>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-    <title>GoogleMapsSimpleGeocode example</title>
-</head>
-<body>
-    <form action="" method="get">
-        <input name="address" size="40" value="<?=$_GET['address']?>" /><br />
-        <input type="submit" value="Искать!" />
-    </form>
-    <pre>
+<?php
 
-    <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
-    require_once 'GoogleMapsSimpleGeocode.php';
+use \dotzero\GMapsGeocode;
+use \dotzero\GMapsException;
 
-    if(isset($_GET['address']) and strlen($_GET['address']) > 2)
-    {
-        $geo = GoogleMapsSimpleGeocode::getInstance();
+try {
+    $result = (new GMapsGeocode(getenv('GOOGLE_API')))
+        ->setAddress('Helsinki')
+        ->setRegion('FI')
+        ->setComponents(array(
+            'route' => 'Annegatan',
+            'administrative_area' => 'Helsinki',
+            'country' => 'Finland'
+        ))
+        ->search();
 
-        $geo->setAddress($_GET['address']);
-        $geo->setApiKey('API_KEY');
-        $geo->setOutput('csv');
-        $geo->setEncoding('utf8');
-
-        if($result = $geo->search())
-        {
-            foreach($result AS $title => $value)
-            {
-                printf('%s - %s<br>', $title, print_r($value, true));
-            }
-        }
-        else
-        {
-            echo '<b>Ошибка:</b> '.$geo->errorMessage();
-        }
-    }
-    ?>
-
-</body>
-</html>
+    print_r($result);
+} catch (GMapsException $e) {
+    printf('Error (%d): %s', $e->getCode(), $e->getMessage());
+}
--- examples/example2.php
+++ examples/example2.php
@@ -1,51 +1,17 @@
-<html>
-<head>
-    <meta content="text/html; charset=utf8" http-equiv="Content-Type"/>
-    <title>GoogleMapsSimpleGeocode example 2</title>
-</head>
-<body>
-    <form action="" method="get">
-        <input name="address" size="40" value="<?=$_GET['address']?>" /><br />
-        <input type="submit" value="Искать!" />
-    </form>
+<?php
 
-    <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
-    require_once 'GoogleMapsSimpleGeocode.php';
+use \dotzero\GMapsGeocodeReverse;
+use \dotzero\GMapsException;
 
-    if(isset($_GET['address']) and strlen($_GET['address']) > 2)
-    {
-        $geo = GoogleMapsSimpleGeocode::getInstance();
+try {
+    $geo = (new GMapsGeocodeReverse('AIzaSyA4gme0AceHhs_3QSknmoiC4FsS3EdK9VE'))
+        ->setLatLng('40.714224', '-73.961452')
+//        ->setPlaceId('ChIJd8BlQ2BZwokRAFUEcm_qrcA')
+        ->search();
 
-        $geo->setAddress($_GET['address']);
-        $geo->setApiKey('API_KEY');
-
-        // CSV
-        $geo->setOutput('csv');
-
-        if($result = $geo->search(true))
-        {
-            echo '<h3>CSV Raw response</h3><textarea rows="5" cols="80" name="text">'.$result.'</textarea>';
-        }
-
-        // JSON
-        $geo->setOutput('json');
-
-        if($result = $geo->search(true))
-        {
-            echo '<h3>JSON Raw response</h3><textarea rows="20" cols="80" name="text">'.$result.'</textarea>';
-        }
-
-        // JSON
-        $geo->setOutput('xml');
-
-        if($result = $geo->search(true))
-        {
-            echo '<h3>XML Raw response</h3><textarea rows="20" cols="80" name="text">'.$result.'</textarea>';
-        }
-
-    }
-    ?>
-
-</body>
-</html>
+    print_r($result);
+} catch (GMapsException $e) {
+    printf('Error (%d): %s', $e->getCode(), $e->getMessage());
+}
